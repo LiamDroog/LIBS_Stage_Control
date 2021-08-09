5a30d95110d94bc122321cd6bd5c1bfa51ec0a16
Liam Droog <droog@ualberta.ca>
2021-08-09 13:10:15 -0600

    Added more cats

--- Camera Control/Disp_Images.py
+++ Camera Control/Disp_Images.py
@@ -2,60 +2,65 @@ import tkinter as tk
 from tkinter import font, ttk
 import os
 from PIL import ImageTk, Image
-class imageViewer:
-    def __init__(self, target_dir, file_extension, master=None):
-        # self.window = tk.Tk(className='\Image Viewer')
-        self.window = tk.Toplevel(master=master)
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+class imageViewer:
+    def __init__(self, image_target_dir, spectra_target_dir, file_extension, master=None):
+        # self.window = tk.Tk(className='\Image Viewer')
+
+        # instatiate master window
+        # below is all gui setup
+        self.window = tk.Toplevel(master=master)
         self.canvasframe = tk.Frame(master=self.window)
-        self.canvas = tk.Canvas(master=self.canvasframe, width=660, height=800)
+        self.canvas = tk.Canvas(master=self.canvasframe, width=1000, height=800)
         self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
 
         self.scrollbar = ttk.Scrollbar(master=self.window, orient='vertical', command=self.canvas.yview)
-        self.scrollFrame =ttk.Frame(master=self.canvas)
+        self.scrollFrame = ttk.Frame(master=self.canvas)
         self.scrollFrame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox('all')))
         self.canvas.create_window((0,0), window=self.scrollFrame, anchor='nw')
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
 
-        self.targetDir = target_dir
+        self.image_target_dir = image_target_dir
+        self.spectra_target_dir = spectra_target_dir
         self.file_ext = file_extension
         self.current_display = None
-        self.dir_list = []
+        self.img_dir_list = []
+        self.spectra_dir_list = []
         self.iter = 0
-        print('sending to poll directory')
+        print('sending to poll image_directory')
         self.pollDirectory()
-
+        # run self.onClosing when we close the window to ensure proper cleanup
         self.window.protocol('WM_DELETE_WINDOW', self.onClosing)
-
+        # L O O P
         self.window.mainloop()
 
     def pollDirectory(self):
-
-        if not os.getcwd() == self.targetDir:
-            os.chdir(self.targetDir)
+        # Make sure cwd is the same as the target wd
+        if not os.getcwd() == self.image_target_dir:
+            os.chdir(self.image_target_dir)
         try:
-            self.dir_list = sorted(filter(os.path.isfile, os.listdir('.')), key=os.path.getmtime)
+            # get a lits of all files in image_directory
+            self.img_dir_list = sorted(filter(os.path.isfile, os.listdir('.')), key=os.path.getmtime)
+            # set current top image to the most recently modified file in the image_directory
+            if not self.current_display or self.current_display != self.img_dir_list[-1]:
+                self.current_display = self.img_dir_list[-1]
 
-            if not self.current_display or self.current_display != self.dir_list[-1]:
-                self.current_display = self.dir_list[-1]
-            self.update_image(self.dir_list)
+            os.chdir(self.spectra_target_dir)
+            self.spectra_dir_list = sorted(filter(os.path.isfile, os.listdir('.')), key=os.path.getmtime)
 
-            # display image yeet
+            self.update_image(self.img_dir_list, self.spectra_dir_list)
 
-            print(self.current_display)
-            # self.iter += 1
-            # if self.iter % 5 == 0:
-            #     self.iter = 0
-            #     self.update_image()
-            #     print('Updating images based on iterations')
         except Exception as e:
             print(e)
         finally:
             self.window.after(1000, self.pollDirectory)
 
 
-        # if [i for i in os.listdir(self.targetDir) if i[-3:] == self.file_ext]:
-        #     present_shots = [int(i[:-4].split('-')[-1]) for i in os.listdir(self.targetDir) if i[-3:] == self.file_ext]
+        # if [i for i in os.listdir(self.image_target_dir) if i[-3:] == self.file_ext]:
+        #     present_shots = [int(i[:-4].split('-')[-1]) for i in os.listdir(self.image_target_dir) if i[-3:] == self.file_ext]
         #     present_shots.sort(key=lambda x: os.path.getmtime(x))
         #     print(present_shots)
 
@@ -63,7 +68,7 @@ class imageViewer:
         self.window.destroy()
 
     def old_update_image(self):
-
+        # deprecated, only displays one image :(
         font = tk.font.Font(family='Helvetica', size=36, weight='bold')
         self.header = tk.Label(master=self.scrollFrame, text=self.current_display.split('\\')[-1], font=font)
         self.header.grid(row=0, column=0, columnspan=2)
@@ -87,13 +92,22 @@ class imageViewer:
 
 
     def _on_mousewheel(self, event):
+        # scroll on command
         self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
 
-    def update_image(self, dir_list):
+    def update_image(self, img_dir_list, spectra_dir_list):
+
+        # init font
         font = tk.font.Font(family='Helvetica', size=16, weight='bold')
-        dir_list = dir_list[-5:]
-        dir_list.reverse()
-        for j, i in enumerate(dir_list):
+        # get 5 most recent files and reverse list so that the most recent is index 0
+        img_dir_list = img_dir_list[-5:]
+        img_dir_list.reverse()
+        spectra_dir_list = spectra_dir_list[-5:]
+        spectra_dir_list.reverse()
+
+        for j, i in enumerate(img_dir_list):
+            os.chdir(self.image_target_dir)
+            # put it into a frame and pack it nicely
             self.rtnFrame = tk.Frame(master=self.scrollFrame, borderwidth=3, relief='groove')
             imgframe = tk.Frame(master=self.rtnFrame)
             specframe = tk.Frame(master=self.rtnFrame)
@@ -103,7 +117,7 @@ class imageViewer:
             self.sample_header.grid(row=0, column=0, columnspan=2)
             # self.imageFrame = tk.Frame(master=self.window)
             self.sample_image = Image.open(i)
-            factor = 0.25
+            factor = 0.35
             self.sample_image = self.sample_image.resize((int(self.sample_image.size[0] * factor), int(self.sample_image.size[1] * factor)),
                                            Image.ANTIALIAS)
             self.sample_tkimage = ImageTk.PhotoImage(self.sample_image)
@@ -112,20 +126,31 @@ class imageViewer:
             self.sample_label.image = self.sample_tkimage
             self.sample_label.grid(row=1, column=0, columnspan=2)
 
-
             # for spectra of sample. NEEDS REWORK FOR SPECRA EH
-            self.spec_header = tk.Label(master=specframe, text='Spectra', font=font)
-            self.spec_header.grid(row=0, column=0, columnspan=2)
-            # self.imageFrame = tk.Frame(master=self.window)
-            self.spec_image = Image.open('C:\\Users\\Liam Droog\\Desktop\\spectra.png')
-            factor = 0.25
-            self.spec_image = self.spec_image.resize((int(self.spec_image.size[0] * factor), int(self.spec_image.size[1] * factor)),
-                                           Image.ANTIALIAS)
-            self.spec_tkimage = ImageTk.PhotoImage(self.spec_image)
 
-            self.spec_label = tk.Label(master=specframe, image=self.spec_tkimage)
-            self.spec_label.image = self.spec_tkimage
-            self.spec_label.grid(row=1, column=0, columnspan=2)
+            os.chdir(self.spectra_target_dir)
+
+            self.spec_header = tk.Label(master=specframe, text=spectra_dir_list[j].split('\\')[-1], font=font)
+            self.spec_header.grid(row=0, column=0, columnspan=2)
+            dat = np.loadtxt(spectra_dir_list[j], dtype=float, delimiter=';')
+            # we need to have the plot shit in here!
+            figure = plt.Figure(figsize=(5,4), dpi=100)
+            ax = figure.add_subplot(111)
+            ax.plot(dat[:,0], dat[:,1])
+            lineplot = FigureCanvasTkAgg(figure, specframe)
+            lineplot.get_tk_widget().grid(row=1, column=0, columnspan=2)
+
+
+
+            # self.spec_image = Image.open('C:\\Users\\Liam Droog\\Desktop\\spectra.png')
+            # factor = 0.25
+            # self.spec_image = self.spec_image.resize((int(self.spec_image.size[0] * factor), int(self.spec_image.size[1] * factor)),
+            #                                Image.ANTIALIAS)
+            # self.spec_tkimage = ImageTk.PhotoImage(self.spec_image)
+            #
+            # self.spec_label = tk.Label(master=specframe, image=self.spec_tkimage)
+            # self.spec_label.image = self.spec_tkimage
+            # self.spec_label.grid(row=1, column=0, columnspan=2)
 
             imgframe.grid(row=0, column=1)
             specframe.grid(row=0, column=0)
@@ -148,5 +173,3 @@ class imageViewer:
 if __name__ == '__main__':
     # imageViewer('C:\\Users\\Liam Droog\\Desktop\\controlTest\\Images', 'bmp')
     pass
-
-
--- Camera Control/Run.bat
+++ Camera Control/Run.bat
@@ -1,2 +1,3 @@
 @echo off
 start py RunCamera.py
+start py "C:\Users\Liam Droog\PycharmProjects\StanfordBox\DG645 Control Gui.py"
